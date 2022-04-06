df037ce11b46cdc23988767a0d6e1c3a4034778e
Chetan Singh <heychetansingh@gmail.com>
2022-04-06 12:34:14 +0530

    Solanapay

---
+++ lib/addresses.ts
@@ -0,0 +1,10 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('6QJZKxbRZDwD745bbEfmsQZ9SLEZJ313VAcB2sWJAFP5') 
+
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+
+// This is your token/coupon address
+export const couponAddress = new PublicKey('CJDcGndtvNjfWpULQvcFTEWga1JioEx1YVGepH6PSoxW')
